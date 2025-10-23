c1131a9c494c948637b36f68f3901609c9469b3a
usma112 <osmanaafi@gmail.com>
2025-10-23 11:55:30 +0500

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,134 @@
+const info = {
+  // ============ MAIN DETAILS ============
+  main: {
+    name: "Usman Aslam",
+    role: "MERN Stack Developer",
+    description:
+      "I am a Full Stack Developer with 1 year of hands-on experience in building responsive and user-friendly web applications using the MERN stack. I specialize in both frontend and backend development with a strong focus on clean, maintainable code and performance optimization. I’m passionate about learning new technologies, solving real-world problems, and contributing to dynamic and collaborative development teams.",
+    photo: "../osman.jpg",
+    email: "osmanaaafi@gmail.com",
+  },
+
+  // ============ SOCIAL LINKS ============
+  socials: {
+    twitter: "",
+    github: "https://github.com/usma1122",
+    linkedin: "www.linkedin.com/in/usman-aslam-8490b8388",
+    instagram: "https://www.instagram.com/lu_writess?igsh=MWZuOGVkMzFjc2R4Yw==",
+    facebook: "",
+  },
+
+  // ============ PROJECTS ============
+  projects: [
+    {
+      title: "Note Taking App",
+      description:
+        "Collaborated on building responsive React applications with real-time features using Firebase. Integrated backend APIs, optimized performance, and contributed to debugging, testing, and feature development.",
+      technologies: "React, Redux , Firebase ",
+      github: "https://github.com/usma1122/Note-taking-app",
+      link: "",
+    },
+
+    {
+      title: "Online Electronics Store",
+      description:
+        "Developed a full-stack Online Electronics Store using the MERN stack. Created an admin panel to manage products, users, and orders. Integrated REST APIs, used Redux Toolkit for state management, and Tailwind CSS for responsive design.",
+      technologies: "MERNStack, Reduxtoolkit, Tailwind ",
+      github: "https://github.com/usma1122/Online-Electronics-Store",
+      link: "",
+    },
+
+    {
+      title: "Food Ordering App",
+      description:
+        "Built a responsive Food Ordering App using React.js, Redux Toolkit, and Tailwind CSS., efficient cart management, and a seamless user experience.",
+      technologies: "React, Redux Toolkit, Tailwind CSS",
+      github: "https://github.com/usma1122/Food-App",
+      link: "",
+    },
+
+    {
+      title: "Expense Tracking App",
+      description:
+        "Chatroom is a simple online chatting web application where you can create chat rooms and have a fun conversation with a group of people.",
+      technologies: "React, CSS, papaparse",
+      github: "https://github.com/usma1122/expense-tracking-app",
+      link: "",
+    },
+    {
+      title: "Todolist App",
+      description:
+        "Built a Todo List web app using React, Redux Toolkit, and Tailwind CSS Users can add, update, and delete tasks, with state management and a responsive UI.",
+      technologies: "React, ReduxToolkit, Tailwind",
+      github: "https://github.com/usma1122/Todolist",
+      link: "",
+    },
+    {
+      title: "Exchange Currency App",
+      description:
+        "Built a Currency Exchange web app using React, Tailwind CSS, and custom hooks Users can convert amounts between currencies, swap currencies, and view real-time conversion rates",
+      technologies: "React, API, Tailwind",
+      github: "https://github.com/usma1122/currency-exchange-app",
+      link: "",
+    },
+  ],
+  // ============ EDUCATION ============
+  education: [
+    {
+      school: "The Islamiya University of Bahawalpur",
+      degree: "Bachelor of Science in Computer Science",
+      duration: "2021 - 2025",
+      image: "uni.png",
+    },
+    {
+      school: "Government Degree College Dera Nawab, Ahmedpur East",
+      degree: "F.Sc Pre-Engineering",
+      duration: "2017 - 2019",
+      image: "college.jpeg",
+    },
+    {
+      school: "Ahmed Pur Public High School, Ahmedpur East",
+      degree: "Bio Group",
+      duration: "2015 - 2017",
+      image: "school.jpg",
+    },
+  ],
+
+  // ============ EXPERIENCE ============
+  experience: [
+    {
+      position: "Junior Frontend Developer",
+      company: "Starzplay",
+      duration: "August 2025 - Present 2025 (3 Months)",
+      image: "starzplay.avif",
+      descriptions: [
+        "Interned at StarzPlay as a Junior Frontend Developer.",
+        "Built responsive UIs with React, Tailwind CSS, Redux, and Redux-Saga.",
+        "Improved performance and user experience through collaboration and debugging.",
+      ],
+    },
+    {
+      position: "Full-Stack Developer (FYP) ",
+      company: "Self Employed",
+      duration: "Mar 2024 - Feb 2019 ",
+      image: "images.png",
+      descriptions: [
+        "Built a full-stack Electronics Store (FYP) using the MERN Stack with an admin panel.",
+        "Used Redux Toolkit and Tailwind CSS for responsive UI and state management.",
+        "Completed independently through self-learning and YouTube tutorials.",
+      ],
+    },
+  ],
+
+  // ============ CONTACT ============
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at osmanaafi@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+  },
+
+  // ============ FOOTER ============
+  footer: "© 2025 Usman Aslam . All Rights Reserved",
+};
+
+export default info;
